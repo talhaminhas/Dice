fcb80478eb10542961162f686875b82537068930
Talha Minhas <i160459@nu.edu.pk>
2020-01-29 14:44:50 +0500

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Talhiii on 29/01/2020.
+//  Copyright © 2020 Talha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
